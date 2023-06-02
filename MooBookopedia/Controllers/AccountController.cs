3aa92cd3a827b1b9aa0ffadac490ba7b5d7af673
Deniorrr <kujonek95@gmail.com>
2023-06-02 11:51:26 +0200

    Created Register View

---
+++ MooBookopedia/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MooBookopedia.Data;
+using MooBookopedia.Data.ViewModels;
+
+namespace MooBookopedia.Controllers
+{
+    public class AccountController : Controller
+    {
+        private AppDbContext _context;
+
+        public AccountController()
+        {
+        }
+        public IActionResult Register()
+        {
+            return View(new RegisterVM());
+        }
+    }
+}
