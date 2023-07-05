5806ae1bf201525bf1497fd9cb4352120a6c5639
Deniorrr <kujonek95@gmail.com>
2023-07-05 20:45:23 +0200

    Deleted unused code

--- MooBookopedia/Controllers/AccountController.cs
+++ MooBookopedia/Controllers/AccountController.cs
@@ -22,31 +22,14 @@ namespace MooBookopedia.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterVM registerVM)
         {
-            if (!ModelState.IsValid) return View(registerVM);
+            if(!ModelState.IsValid) return View(registerVM);
 
-            //var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
-            //if (user != null)
-            //{
-            //TempData["Error"] = "This email address is already in use";
-            //return View(registerVM);
-            //}
             if(!DataBaseAccess.CreateAccount(registerVM.FullName, registerVM.EmailAddress, registerVM.Password))
             {
                 TempData["Error"] = "This username or email is already in use";
                 return View(registerVM);
             }
             return View("~/Views/Home/Index.cshtml");
-            //var newUser = new ApplicationUser()
-            //{
-            //    FullName = registerVM.FullName,
-            //    Email = registerVM.EmailAddress,
-            //    UserName = registerVM.EmailAddress
-            //};
-            //var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
-
-            //if (newUserResponse.Succeeded)
-            //    await _userManager.AddToRoleAsync(newUser, UserRoles.User);
-
 
         }
 
