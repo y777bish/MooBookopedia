a0122cf2612329779f44ddcb758e6a9d5c918ffb
Denis Poczęty <49251639+Deniorrr@users.noreply.github.com>
2023-07-06 16:26:09 +0200

    Login works, example of usage shown in _Layout.cshtml

--- MooBookopedia/Controllers/AccountController.cs
+++ MooBookopedia/Controllers/AccountController.cs
@@ -28,13 +28,35 @@ namespace MooBookopedia.Controllers
                 TempData["Error"] = "This username or email is already in use";
                 return View(registerVM);
             }
-            return View("~/Views/Home/Index.cshtml");
-
+            return View("~/Views/Account/Login.cshtml");
+            
         }
 
         public IActionResult Login()
         {
             return View(new LoginVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            int userId = DataBaseAccess.Login(loginVM.EmailAddress, loginVM.Password);
+
+            if (userId == -1)
+            {
+                TempData["Error"] = "Wrong login or password";
+                return View(loginVM);
+            }
+            DataBaseAccess.AddSession(userId);
+            return View("~/Views/Home/Index.cshtml");
+
+        }
+        public IActionResult Logout()
+        {
+            DataBaseAccess.Logout();
+            return View("~/Views/Home/Index.cshtml");
         }
     }
 }
